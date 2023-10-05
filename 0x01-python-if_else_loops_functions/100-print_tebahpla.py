bc6c2516c160c9e869be650b78f7fb1d82d8d6a0
Eric <stephen.erich243@gmail.com>
2023-10-05 07:03:33 -0700

    task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for alpha in range(ord('z'), ord('a') - 1, -1):
+    if alpha % 2 == 0:
+        print('{:s}'.format(chr(alpha)).upper(), end='')
+    else:
+        print('{:s}'.format(chr(alpha)), end='')
