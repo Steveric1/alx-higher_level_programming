a22969dc22d48a30d40266c674eaae7b4c8e1cfb
Eric <stephen.erich243@gmail.com>
2023-10-05 18:44:04 -0700

    modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] == '_' and dir(hidden_4)[i][1] == '_':
+            continue
+        print(dir(hidden_4)[i])
