3f39119ff725eb0d2d7e532654b4c6cd37f5ed2f
Eric <stephen.erich243@gmail.com>
2023-10-09 11:52:27 -0700

    Task: List Tuples

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,9 @@
 
 def print_reversed_list_integer(my_list=[]):
 
+    if len(my_list) == 0:
+        return None
+
     reverse_list = reversed(my_list)
     for r in reverse_list:
         print("{:d}".format(r))
