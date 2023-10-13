f5ae238b3e1f334268a30314e703c4cfce699f27
Eric <stephen.erich243@gmail.com>
2023-10-13 09:12:27 -0700

    Set and Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
 
-    new_matrix = [[row[i] for row in matrix]for i in range(len(matrix))]
+    new_matrix = [[0 for _ in range(len(row))]for row in matrix]
     for i in range(len(new_matrix)):
         for j in range(len(new_matrix[i])):
             new_matrix[i][j] = matrix[i][j]
