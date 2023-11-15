e0d726e814c26ce21b47007022a28664ccd620bc
Eric <stephen.erich243@gmail.com>
2023-11-14 18:08:03 -0800

    ooop

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,77 +1,81 @@
 #!/usr/bin/python3
-
-"""Define classes for a singly-linked list."""
+"""Node and Singlylinkedlist class"""
 
 
 class Node:
-    """Represent a node in a singly-linked list."""
+    """A class Node"""
 
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
+        """Initialize the Node class with two fields
         Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
+            data(int): the node data
+            next_node(Node): next node to help in traversing the linkedlist
+            """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+        """Get or retrieve a value"""
+        return self.__data
 
     @data.setter
     def data(self, value):
-        if not isinstance(value, int):
+        """Set a value"""
+        if type(value) is not int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
+        """Set the next_node value"""
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Represent a singly-linked list."""
+    """Define a singlylinkedlist"""
 
-    def __init__(self):
-        """Initalize a new SinglyLinkedList."""
-        self.__head = None
-
-    def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
-        The node is inserted into the list at the correct
-        ordered numerical position.
+    def __init__(self, head=None):
+        """Initialize the singlylinkedlist with head fields
         Args:
-            value (Node): The new Node to insert.
-        """
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+            head: the head of the singlylinkedlist
+            """
+        self.__head = head
 
     def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+        """Return the string representation of the singlylinkedlist"""
+        current = self.__head
+        result = ""
+        while current is not None:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result.lstrip("\n")
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted position."""
+        new_node = Node(value)
+
+        """Check if the head is empty and if value is smaller than the
+        head, insert at the beginning"""
+
+        if self.__head is None:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            """Traverse the list to find the correct position"""
+            while (current.next_node is not None
+                    and current.next_node.data < value):
+                current = current.next_node
+
+            new_node.next_node = current.next_node
+            current.next_node = new_node
