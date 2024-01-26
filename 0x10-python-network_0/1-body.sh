8ca05d897c2863d965fb424b8cc1bb7464138f37
Eric <stephen.erich243@gmail.com>
2024-01-26 17:48:11 +0100

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# GET request to the URL, and displays the body of the response of 200 status code.
+# Get the response body for a given URL for 200 status code responses.
 curl -sL "$1"
