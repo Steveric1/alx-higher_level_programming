d90bb37eb33b0d48db9fc1ce7ce3703c46e12203
Eric <stephen.erich243@gmail.com>
2024-01-29 09:12:08 +0100

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+if "__name__" == "__main__":
+    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
