05d2318595f6f03cf9f6e30d58aa634d30bb225e
Eric <stephen.erich243@gmail.com>
2024-02-21 10:15:08 +0100

    js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  if (response.statusCode !== 200) {
+    console.log('Invalid:', response.statusCode);
+  }
+
+  let count = 0;
+  try {
+    const films = JSON.parse(body).results;
+    films.forEach(film => {
+      const characters = film.characters;
+      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    });
+    console.log(count);
+  } catch (parseError) {
+    console.log(parseError);
+  }
+});
---
+++ 0x14-javascript-web_scraping/ex.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const array = [1, 2, 3, 4, 5];
+
+array.forEach(function(element, index, array) {
+    console.log(`Element: ${element}, Index: ${index}`);
+});
