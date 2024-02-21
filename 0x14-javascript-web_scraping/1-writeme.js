1def48add605b0704079ab2f0c314bdaaae20688
Eric <stephen.erich243@gmail.com>
2024-02-21 01:59:44 +0100

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.log('Usage: 1-writeme.js <file> <text>');
-  process.exit(1);
-}
-
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (error) => {
   if (error) {
     console.log(error);
